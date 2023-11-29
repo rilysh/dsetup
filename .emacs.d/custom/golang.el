004ab4eba5646a64baf6cb6da6be0e95dc5b5857
rilysh <nightquick@proton.me>
2023-11-29 12:43:39 -0500

    migrate from unlicense to GPLv3

--- .emacs.d/custom/golang.el
+++ .emacs.d/custom/golang.el
@@ -1,3 +1,14 @@
+(require 'lsp-mode)
+(add-hook 'go-mode-hook #'lsp-deferred)
+
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+
 (defun set-exec-path-from-shell-PATH ()
   (let ((path-from-shell (replace-regexp-in-string
                           "[ \t\n]*$"
