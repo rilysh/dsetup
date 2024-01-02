b03529d71565a39c741729014dfbf0ccb30d4c83
rilysh <nightquick@proton.me>
2024-01-02 20:35:59 +0530

    add missing files

---
+++ .emacs.d/custom/auxiliary.el
@@ -0,0 +1,8 @@
+;; Don't write auxelary files to current directory. Use a specified directory instead.
+;; Taken from: https://www.gnu.org/software/emacs/manual/html_node/efaq/Not-writing-files-to-the-current-directory.html
+(setq lock-file-name-transforms
+      '(("\\`/.*/\\([^/]+\\)\\'" "~/.emacs.d/aux/\\1" t)))
+(setq auto-save-file-name-transforms
+      '(("\\`/.*/\\([^/]+\\)\\'" "~/.emacs.d/aux/\\1" t)))
+(setq backup-directory-alist
+      '((".*" . "~/.emacs.d/aux/")))
